f34f82b1af4e68d0406b3c34f601ab170112802a
Raúl Fabra <fabraraul@yahoo.com>
2024-07-15 20:53:31 +0200

    Cambios en el buscador de favoritos

--- src/components/searchBarFavourites.jsx
+++ src/components/searchBarFavourites.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react'
+
 export const SearchBarFavourites = () => {
-  const handleSubmit = () => {}
-  const handleChange = () => {}
+  const [keyword, setKeyword] = useState()
+
+  const handleSubmit = (event) => {
+    event.preventdefault()
+  }
+
+  const handleChange = (event) => {
+    setKeyword(event.target.value)
+  }
   return (
     <section className='searchBar'>
       <form onSubmit={handleSubmit} className='searchBar__container'>
-        <input type='text' name='booking' id='booking' className='searchBar__container--input' placeholder='Search photos' onChange={handleChange} />
+        <input type='text' name='myphotos' id='myphotos' className='searchBar__container--input' placeholder='Search photos' onChange={handleChange} value={keyword} />
         <img src='public\magnifying-glass.png' alt='search__icon' className='searchBar__container--iconGlass' width={25} height={25} />
       </form>
     </section>
